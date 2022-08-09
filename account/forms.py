3b90432bb86969665cc5dc3b989f03154670492d
rohitb1306 <rohit@webllisto.com>
2022-08-09 14:28:53 +0530

    changes in forms.py

--- account/forms.py
+++ account/forms.py
@@ -43,6 +43,13 @@ class signupform(forms.ModelForm):
             ),
 
         }
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
 class updateprofileform(forms.ModelForm):
     class Meta:
         model = MyUser
